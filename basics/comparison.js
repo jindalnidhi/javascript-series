68401c47f68c9b5f44e6da3b2b6aaea4735c30af
jindalnidhi <161034409+jindalnidhi@users.noreply.github.com>
2024-09-11 13:12:13 +0000

    comparison

---
+++ basics/comparison.js
@@ -0,0 +1,37 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+// === 
+
+console.log("2" === 2);
+//type of val---> result
+//undefined-->undefine
+//null-->object
+//boolean-->boolean
+//number-->number
+//string-->string
+//function-->function
+//object-->object
+//== --> equal to
+//=== --> equal value and equal type
+//!= --> not equal
+//!== --> not equal value or not equal type
+//> --> gretaer than
+// <  --> less than
+//<= --> gretaer than or equal to
+// >= -->less tha or equal to
